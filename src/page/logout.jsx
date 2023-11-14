990f519e996fe09fdcf7588a2e219e54d6230d8e
BoatGT-9 <twz192545@gmail.com>
2023-11-15 00:57:49 +0700

    time

--- src/page/logout.jsx
+++ src/page/logout.jsx
@@ -4,13 +4,14 @@ import authService from "../service/auth.service";
 
 const logout = () => {
   const navigate = useNavigate();
+  
   const handleLogout = () => {
     authService.logout();
+    setTimeout(() => {
+      handleLogout();
+    },1000);
     navigate("/");
   };
-  setTimeout(() => {
-    handleLogout();
-  },);
 
   return (
   <div> Logout</div>
